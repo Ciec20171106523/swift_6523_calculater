df250a4fe9fae7c63791beb937c0a596529343bd
wangtongwu <892280057@qq.com>
2018-12-05 09:41:42 +0800

    Ciec_62523
    更新版连加计算器

--- 计算器/ViewController.swift
+++ 计算器/ViewController.swift
@@ -5,87 +5,295 @@
 //  Created by s20171106523 on 2018/10/27.
 //  Copyright © 2018 s20171106523. All rights reserved.
 //
-
 import UIKit
-var temp:Double = 0
-var operateorFlag = 0
+class Stack
+{
+    var tam=0
+}
+
 class ViewController: UIViewController {
     @IBOutlet  weak var LED:UITextField!
+    var temp:Double = 0
+    var tempflag=0
+    var d=0
+    var m_temp:Double=0
+    var N=0
     @IBAction func BUTTON1(_ sender: Any) {
-        LED.text = LED.text! + "1"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "1"
+        }
+        else
+        {
+            LED.text = LED.text! + "1"
+        }
     }
     @IBAction func BUTTON2(_ sender: Any) {
-        LED.text = LED.text! + "2"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "2"
+        }
+        else
+        {
+            LED.text = LED.text! + "2"
+        }
     }
     @IBAction func BUTTON3(_ sender: Any) {
-        LED.text = LED.text! + "3"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "3"
+        }
+        else
+        {
+            LED.text = LED.text! + "3"
+        }
     }
     @IBAction func BUTTON4(_ sender: Any) {
-        LED.text = LED.text! + "4"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "4"
+        }
+        else
+        {
+            LED.text = LED.text! + "4"
+        }
     }
     @IBAction func BUTTON5(_ sender: Any) {
-        LED.text = LED.text! + "5"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "5"
+        }
+        else
+        {
+            LED.text = LED.text! + "5"
+        }
     }
     @IBAction func BUTTON6(_ sender: Any) {
-        LED.text = LED.text! + "6"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "6"
+        }
+        else
+        {
+            LED.text = LED.text! + "6"
+        }
     }
     @IBAction func BUTTON7(_ sender: Any) {
-        LED.text = LED.text! + "7"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "7"
+        }
+        else
+        {
+            LED.text = LED.text! + "7"
+        }
     }
     @IBAction func BUTTON8(_ sender: Any) {
-        LED.text = LED.text! + "8"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "8"
+        }
+        else
+        {
+            LED.text = LED.text! + "8"
+        }
     }
     @IBAction func BUTTON9(_ sender: Any) {
-        LED.text = LED.text! + "9"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "9"
+        }
+        else
+        {
+            LED.text = LED.text! + "9"
+        }
     }
     @IBAction func BUTTON0(_ sender: Any) {
-        LED.text = LED.text! + "0"
+        if(tempflag==5)
+        {
+            LED.text=""
+            tempflag=0
+        }
+        if(N>0)
+        {
+            LED.text=""
+            LED.text = LED.text! + "0"
+        }
+        else
+        {
+            LED.text = LED.text! + "0"
+        }
     }
     @IBAction func BUTTONDUT(_ sender: Any) {
         LED.text = LED.text! + "."
     }
     
     
-    @IBAction func ButtonAdd(_ sender: Any) {
-        temp = Double(LED.text!)!
-        LED.text = ""
-        operateorFlag = 1
+    @IBAction func ButtonAdd(_ sender: Any)
+    {
+        tempflag=1
+        temp = temp + Double(LED.text!)!
+        m_temp = m_temp + Double(LED.text!)!
+        if(N>0)
+        {
+            LED.text="\(m_temp)"
+        }
+        else
+        {
+            LED.text = ""
+        }
+        N=N+1
+        
     }
-    @IBAction func ButtonSub(_ sender: Any) {
-        temp = Double(LED.text!)!
-        LED.text = ""
-        operateorFlag = 2
+    @IBAction func ButtonSub(_ sender: Any)
+    {
+        tempflag=2
+        if(N>0)
+        {
+            temp = temp - Double(LED.text!)!
+            m_temp = m_temp - Double(LED.text!)!
+            LED.text="\(m_temp)"
+        }
+        else
+        {
+            temp = Double(LED.text!)!
+            m_temp = Double(LED.text!)!
+            LED.text = ""
+        }
+        N=N+1
     }
+    
     @IBAction func BUTTONCHEN(_ sender: Any) {
-        temp = Double(LED.text!)!
-        LED.text = ""
-        operateorFlag = 3
-    }
+        tempflag=3
+        if(N>0)
+        {
+            temp = temp * Double(LED.text!)!
+            m_temp = m_temp * Double(LED.text!)!
+            LED.text="\(m_temp)"
+        }
+        else
+        {
+            temp = Double(LED.text!)!
+            m_temp =  Double(LED.text!)!
+            LED.text = ""
+        }
+        N=N+1
+}
     @IBAction func BUTTONCHU(_ sender: Any) {
-        temp = Double(LED.text!)!
-        LED.text = ""
-        operateorFlag = 4
-    }
+        tempflag=4
+        if(N>0)
+        {
+            
+            temp = temp / Double(LED.text!)!
+            m_temp = m_temp / Double(LED.text!)!
+            LED.text="\(m_temp)"
+        }
+        else
+        {
+            temp = Double(LED.text!)!
+            m_temp = Double(LED.text!)!
+            LED.text = ""
+        }
+        N=N+1
+}
     @IBAction func BUTTONAC(_ sender: Any) {
         LED.text = ""
+        temp=0
+        m_temp=0
+        N=0
+    }
+    @IBAction func BUTTONREM(_ sender: Any) {
+        temp=Double(LED.text!)!
+        LED.text=""
+        temp=temp/100
+        LED.text="\(temp)"
+    }
+    @IBAction func BUTTONC(_ sender: Any) {
+        temp=Double(LED.text!)!
+        d = Int(temp)
+        d = d/10
+        temp=Double (d)
+        LED.text="\(d)"
+    }
+    
+    @IBAction func BUTTONNEG(_ sender: Any) {
+        temp=Double(LED.text!)!
+        LED.text=""
+        temp = -temp
+        LED.text="\(temp)"
     }
     @IBAction func BUTTONResult(_ sender: Any) {
-        if operateorFlag == 1{
+        if (tempflag == 1){
             temp = temp + Double(LED.text!)!
             LED.text = "\(temp)"
         }
-        if operateorFlag == 2{
+        if (tempflag == 2){
             temp = temp - Double(LED.text!)!
             LED.text = "\(temp)"
         }
-        if operateorFlag == 3{
+        if (tempflag == 3){
             temp = temp * Double(LED.text!)!
             LED.text = "\(temp)"
         }
-        if operateorFlag == 4{
+        if (tempflag == 4){
             temp = temp / Double(LED.text!)!
             LED.text = "\(temp)"
         }
+        tempflag=5
     }
     
     
@@ -93,7 +301,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
-
